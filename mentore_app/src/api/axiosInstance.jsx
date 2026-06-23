cc0ff35e763ff3d4509c1170cabd6fe7b5a9ee19
Shaikh Mustkim <yourgmail@gmail.com>
2026-06-23 18:49:44 +0530

    axios update

--- mentore_app/src/api/axiosInstance.jsx
+++ mentore_app/src/api/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // import.meta.env.VITE_API_BASE_URL
+  baseURL: "http://localhost:5000/api",
   withCredentials: true,
 
   //   headers: {
